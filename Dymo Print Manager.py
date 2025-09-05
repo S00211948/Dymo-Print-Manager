a4f0e9071d8e1e749b7aece0b24b3bcf479fee7a
Shane <sha02ne@gmail.com>
2025-09-05 16:31:36 +0100

    first commit

---
+++ Dymo Print Manager.py
@@ -0,0 +1,277 @@
+import tkinter as tk
+from tkinter import StringVar, ttk, filedialog, messagebox
+import csv
+from typing import List
+from SendToPrint import DymoPrintService
+
+class Employee():
+    Employee = ""
+    Guest_1 = ""
+    Guest_2 = ""
+    Guest_3 = ""
+    Tour = ""
+
+    # constructor
+    def __init__(self, emp, g1, g2, g3, tour):
+        self.Employee = emp
+        self.Guest_1 = g1
+        self.Guest_2 = g2
+        self.Guest_3 = g3
+        self.Tour = tour
+
+    def updateDetails(self, emp, g1, g2, g3, tour):
+        self.Employee = emp
+        self.Guest_1 = g1
+        self.Guest_2 = g2
+        self.Guest_3 = g3
+        self.Tour = tour
+    
+    def __getitem__(self, key):
+        return {"Employee": self.Employee, "Guest_1": self.Guest_1, "Guest_2": self.Guest_2, "Guest_3": self.Guest_3, "Tour": self.Tour}[key]
+
+
+class DymoPrintManager(tk.Tk):
+    printer = DymoPrintService()
+    tour_options=["None"]
+    
+    def __init__(self):
+        super().__init__()
+
+        # Window dimensions
+        w = 400
+        h = 500
+
+        # get screen width and height
+        ws = self.winfo_screenwidth() # width of the screen
+        hs = self.winfo_screenheight() # height of the screen
+
+        # calculate x and y coordinates for the Tk root window
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+
+        # set the dimensions of the screen 
+        # and where it is placed
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        
+        # Set the title of the window
+        self.title("Dymo Print Manager")
+
+        #Configure grid for widget placement
+        self.configure_grid()
+
+        # Create and place the widgets
+        self.setup_widgets()
+
+        # List to store contacts
+        self.employees: List[Employee]= []
+
+    def configure_grid(self):
+        # Configure the rows and columns to stretch
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+
+    def setup_widgets(self):
+        # Search Field
+        self.search_entry = ttk.Entry(self)
+        self.search_entry.grid(column=0, row=0, columnspan=2, padx=10, pady=5, sticky='EW')
+
+        # Search Button
+        self.add_button = ttk.Button(self, text="Search", command=self.find_employee)
+        self.add_button.grid(column=2, row=0, pady=5, padx=10, sticky='EW')
+
+        # Filter By Tour Label
+        self.filter_by_tour_label = ttk.Label(self, text="Filter By Tour:").grid(column=0, row=2, padx=10, pady=5, sticky='W')
+
+        # Filter By Tour Menu
+        self.selected_option = StringVar()
+        self.selected_option.set(self.tour_options[0])
+        self.filter_by_tour_menu = ttk.OptionMenu(self,self.selected_option,*self.tour_options)
+        self.filter_by_tour_menu.grid(column=2, row=2, padx=10, pady=5, sticky='EW')
+        
+        self.file_button = ttk.Button(self, text="Load From CSV", command=self.choose_file)
+        self.file_button.grid(column=0, row=4, padx=10, pady=10, sticky='W')
+
+        self.print_button = ttk.Button(self, text="Print List", command=self.print_labels_from_listbox)
+        self.print_button.grid(column=2, row=4, padx=10, pady=10, sticky='E')
+
+        # Listbox to display contact names
+        self.employee_listbox = tk.Listbox(self)
+        self.employee_listbox.grid(column=0, row=3, columnspan=3, padx=10, pady=10, sticky="WESN")
+        self.employee_listbox.bind("<<ListboxSelect>>", self.edit_contact_details)
+
+
+    def choose_file(self):
+        # Open file dialog to select a file
+        file_path = filedialog.askopenfilename()
+
+        # If a file is selected, update the label
+        if file_path:
+            postfix = file_path.split('.')[-1]
+            if(postfix=='csv'):    
+                self.employees = parse_csv(file_path)
+                self.update_tour_options()
+            else:
+                messagebox.showerror('Error', 'The file type you selected is not supported. Select a .csv file.')
+            self.refresh_listbox()
+        else:
+            self.file_label.config(text="No file selected")
+
+    ### Edit and Import Window Logic
+    def edit_contact_details(self, event):
+        # Get the selected contact index
+        selection = event.widget.curselection()
+        if not selection:
+            return
+        index = selection[0]
+        contact = self.employees[index]
+
+        # Create a new Toplevel window for editing
+        edit_window = tk.Toplevel(self)
+        edit_window.title("Edit Employee Details")
+
+        # Window dimensions
+        w = 300
+        h = 200
+
+        # get screen width and height
+        ws = self.winfo_screenwidth() # width of the screen
+        hs = self.winfo_screenheight() # height of the screen
+
+        # calculate x and y coordinates for the Tk root window
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+
+        edit_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        # Configure edit window grid
+        edit_window.columnconfigure(0, weight=1)
+        edit_window.columnconfigure(1, weight=1)
+        edit_window.columnconfigure(2, weight=1)
+
+        # Labels and Entry widgets for editing the contact details
+        tk.Label(edit_window, text="Employee:").grid(column=0, row=0, padx=10, pady=5, sticky='W')
+        employee_entry = ttk.Entry(edit_window)
+        employee_entry.grid(column=1, row=0, columnspan=2, padx=10, pady=5, sticky='EW')
+        employee_entry.insert(0, str(contact.Employee))
+
+        tk.Label(edit_window, text="Guest 1:").grid(column=0, row=1, padx=10, pady=5, sticky='W')
+        guest1_entry = ttk.Entry(edit_window)
+        guest1_entry.grid(column=1, row=1, columnspan=2, padx=10, pady=5, sticky='EW')
+        guest1_entry.insert(0, str(contact.Guest_1))
+
+        tk.Label(edit_window, text="Guest 2:").grid(column=0, row=2, padx=10, pady=5, sticky='W')
+        guest2_entry = ttk.Entry(edit_window)
+        guest2_entry.grid(column=1, row=2, columnspan=2, padx=10, pady=5, sticky='EW')
+        guest2_entry.insert(0, contact.Guest_2)
+
+        tk.Label(edit_window, text="Guest 3:").grid(column=0, row=3, padx=10, pady=5, sticky='W')
+        guest3_entry = ttk.Entry(edit_window)
+        guest3_entry.grid(column=1, row=3, columnspan=2, padx=10, pady=5, sticky='EW')
+        guest3_entry.insert(0, contact.Guest_3)
+
+        tk.Label(edit_window, text="Tour:").grid(column=0, row=4, padx=10, pady=5, sticky='W')
+        tour_entry = ttk.Entry(edit_window)
+        tour_entry.grid(column=1, row=4, columnspan=2, padx=10, pady=5, sticky='EW')
+        tour_entry.insert(0, contact.Tour)
+
+        # Update Button
+        update_button = ttk.Button(edit_window, text="Update", command=lambda: self.update_contact(index, employee_entry.get(), guest1_entry.get(), guest2_entry.get(), guest3_entry.get(), tour_entry.get(), edit_window))
+        update_button.grid(column=0, row=5, pady=10)
+
+        # Delete Button
+        delete_button = ttk.Button(edit_window, text="Delete", command=lambda: self.delete_contact(index, edit_window))
+        delete_button.grid(column=1, row=5, pady=10)
+
+        # Print Button
+        delete_button = ttk.Button(edit_window, text="Print", command=lambda: self.printer.printLabelList([contact]))
+        delete_button.grid(column=2, row=5, pady=10)
+
+    ### Listbox Entry Handler Functions
+    def update_contact(self, index, employee, guest1, guest2, guest3, tour, edit_window):
+        # Update the contact details in the contacts list
+        self.employees[index] = Employee(employee, guest1, guest2, guest3, tour)
+
+        # Refresh the Listbox to reflect the changes
+        self.refresh_listbox()
+
+        # Close the edit window
+        edit_window.destroy()
+
+    def delete_contact(self, index, edit_window):
+        # Delete the contact at the provided index
+        self.employees.pop(index)
+
+        # Refresh the Listbox to reflect the changes
+        self.refresh_listbox()
+
+        # Close the edit window
+        edit_window.destroy()
+
+    def refresh_listbox(self):
+        # Clear the Listbox
+        self.employee_listbox.delete(0, tk.END)
+
+        # Sort Contacts
+        self.employees.sort(key=lambda emp: emp.Employee)
+        
+        # Insert all contacts into the Listbox
+        for emp in self.employees:
+            self.employee_listbox.insert(tk.END, f"{emp.Employee}")
+
+    def search_listbox(self, value, param):
+        if param == "Name":
+            self.selected_option.set("None")
+            self.employee_listbox.delete(0, tk.END)
+            results = list(filter(lambda emp: str.lower(value) in str.lower(emp.Employee), self.employees))
+            for emp in results:
+                self.employee_listbox.insert(tk.END, f"{emp.Employee}")
+        elif param == "Tour":
+            self.search_entry.delete(0,tk.END)
+            self.employee_listbox.delete(0, tk.END)
+            results = list(filter(lambda emp: emp.Tour == value, self.employees))
+            for emp in results:
+                self.employee_listbox.insert(tk.END, f"{emp.Employee}")
+
+    def find_employee(self):
+        self.search_listbox(self.search_entry.get(),"Name")
+
+    def update_selected_option(self,opt):
+        self.selected_option.set(opt)
+        if opt != "None":
+            self.search_listbox(self.selected_option.get(),"Tour")
+        else:
+            self.refresh_listbox()
+
+    def update_tour_options(self):
+        tours=['None']
+        for e in self.employees:
+            if e.Tour not in tours:
+                tours.append(e.Tour)
+        self.tour_options = tours.sort()
+        self.filter_by_tour_menu.option_clear()
+        menu = self.filter_by_tour_menu["menu"]
+        for t in tours:
+            menu.add_command(label=t,command=lambda value=t: self.update_selected_option(value))
+
+    ### Print Handler Functions
+    def print_labels_from_listbox(self):
+        listbox_entries = self.employee_listbox.get(0,tk.END)
+        entries_to_print=[]
+        for e in listbox_entries:
+            entries_to_print.append(next(filter(lambda emp: str.lower(e) in str.lower(emp.Employee), self.employees),None))
+        self.printer.printLabelList(entries_to_print)
+
+### Data Handler Functions
+def parse_csv(file_path):
+    employees = []
+    for row in csv.DictReader(open(file_path, encoding='utf-8-sig')):
+        employees.append(Employee(f"{row['First_Name']} {row['Surename']}",row['Guest_1'],row['Guest_2'],row['Guest_3'],row['Tour_Number']))
+    
+    return employees
+
+
+if __name__ == "__main__":
+    app = DymoPrintManager()
+    app.mainloop()
---
+++ SendToPrint.py
@@ -0,0 +1,37 @@
+import win32com.client
+
+class DymoPrintService():
+    # Create DYMO COM object
+    label = None
+    labelText = None
+
+    
+    def __init__(self):
+        self.label = win32com.client.Dispatch("Dymo.DymoAddIn")
+        self.labelText = win32com.client.Dispatch("Dymo.DymoLabels")
+        # Load the template
+        if not self.label.Open(r"C:\Users\O89301\OneDrive - The Coca-Cola Company\Documents\DYMO Label\Labels\OpenDay-Basic.label"):
+            raise Exception("Could not open label template")
+
+    def printLabelList(self,dataObj):
+        for d in dataObj:
+            # Format data for printing
+            for l in self.formatForPrinting(d):
+                if l != False:
+                    print(l)  
+                    self.labelText.SetField("Employee", l["Employee_Name"])
+                    self.labelText.SetField("Visitor", l["Visitor_Name"])
+                    self.labelText.SetField("Tour", l["Tour_Number"])
+                    self.label.StartPrintJob()
+                    self.label.Print(1, False)   # 1 copy, not asynchronously
+                    self.label.EndPrintJob()
+
+    def formatForPrinting(self,dataObject):
+        try:
+            res_data=[]
+            for i in range(1,4):
+                if isinstance(dataObject[f'Guest_{i}'],str) and dataObject[f'Guest_{i}'] != '':
+                    res_data.append({"Employee_Name":f"{dataObject['Employee']}","Visitor_Name":dataObject[f'Guest_{i}'],"Tour_Number":dataObject['Tour']})
+            return res_data
+        except Exception as e:
+            return e
